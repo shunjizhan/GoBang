7f944e69ef9c029523b75339876d9473b74b90ea
shunjizhan <shunjizhan@umail.ucsb.edu>
2017-03-07 20:27:18 -0800

    modified print board

--- src/Player.java
+++ src/Player.java
@@ -45,11 +45,20 @@ class ChessBoard {
 	public void printBoard() {
 		for (int i = 0; i < this.size; i++) {
 			for (int j = 0; j < this.size; j++) {
-				System.out.print(this.chess[i][j]);
-				System.out.print(' ');
+				printSingleChess(this.chess[i][j]);	
 			}
 			System.out.println(' ');
 		}
+	}
+
+	public void printSingleChess(int chess) {
+		if (chess == 1)
+			System.out.print("O");
+		else if (chess == -1)
+			System.out.print("X");
+		else
+			System.out.print("-");
+		System.out.print(' ');
 	}
 
 
