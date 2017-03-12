14ac02203ebd237d76bbbcc81816d1a78f1e596e
shunjizhan <shunjizhan@umail.ucsb.edu>
2017-03-11 16:26:12 -0800

    1

--- src/Player.java
+++ src/Player.java
@@ -3,13 +3,16 @@ import java.lang.Character;
 
 public class Player {
 	public int color;			// 1 is blck, -1 is white
+	public boolean isAI;
 
 	public Player() {
 		this.color = 1;
+		this.isAI = false;
 	}
 
 	public Player(int color) {
 		this.color = color;
+		this.isAI = false;
 	}
 }
 
